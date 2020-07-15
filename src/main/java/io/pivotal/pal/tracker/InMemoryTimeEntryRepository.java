37cd4faf33df444cd3518f42b57cff17c3d20408
Kaywin Dcosta <kaywin.dcosta@gmail.com>
2020-07-15 15:39:56 +0000

    updated files

--- src/main/java/io/pivotal/pal/tracker/InMemoryTimeEntryRepository.java
+++ src/main/java/io/pivotal/pal/tracker/InMemoryTimeEntryRepository.java
@@ -1,4 +1,60 @@
 package io.pivotal.pal.tracker;
 
-public class InMemoryTimeEntryRepository {
+import java.util.ArrayList;
+import java.util.List;
+
+public class InMemoryTimeEntryRepository implements TimeEntryRepository {
+
+
+    public List<TimeEntry> timeEntries= new ArrayList<>();
+    public long nextId=1;
+
+    @Override
+    public TimeEntry create(TimeEntry timeEntry) {
+
+        TimeEntry entry=new TimeEntry(nextId++,timeEntry.getProjectId(),timeEntry.getUserId(),timeEntry.getDate(),timeEntry.getHours());
+        timeEntries.add(entry);
+        return entry;
+    }
+
+    @Override
+    public TimeEntry find(long timeEntryId) {
+        return timeEntries.stream()
+                .filter(id -> id.getId() == timeEntryId)
+                .findAny()
+                .orElse(null);
+    }
+
+    @Override
+    public List<TimeEntry> list() {
+        return timeEntries;
+    }
+
+    @Override
+    public TimeEntry update(long timeEntryId, TimeEntry timeEntry) {
+
+//        TimeEntry entry=timeEntries.stream()
+//                .filter(id -> id.getId() == timeEntryId)
+//                .findAny().orElse(null);
+        for (TimeEntry entry : timeEntries) {
+            if(entry.getId()==timeEntryId){
+                entry.setProjectId(timeEntry.getProjectId());
+                entry.setDate(timeEntry.getDate());
+                entry.setHours(timeEntry.getHours());
+                entry.setUserId(timeEntry.getUserId());
+                return entry;
+            }
+
+        }
+
+
+        return null;
+    }
+
+    @Override
+    public void delete(long timeEntryId) {
+        timeEntries.remove(timeEntries.stream()
+                .filter(id -> id.getId() == timeEntryId)
+                .findAny().orElse(null));
+    }
 }
--- src/main/java/io/pivotal/pal/tracker/TimeEntryController.java
+++ src/main/java/io/pivotal/pal/tracker/TimeEntryController.java
@@ -1,4 +1,51 @@
 package io.pivotal.pal.tracker;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+@RestController
 public class TimeEntryController {
+
+    private TimeEntryRepository timeEntryRepository;
+
+    public TimeEntryController(TimeEntryRepository timeEntryRepository) {
+        this.timeEntryRepository=timeEntryRepository;
+    }
+    @PostMapping("/time-entries")
+    public ResponseEntity <TimeEntry>create(@RequestBody TimeEntry timeEntryToCreate) {
+        return new ResponseEntity<TimeEntry>(timeEntryRepository.create(timeEntryToCreate), HttpStatus.CREATED);
+    }
+
+    @GetMapping("/time-entries/{timeEntryId}")
+    public ResponseEntity<TimeEntry> read(@PathVariable long timeEntryId) {
+        TimeEntry result =timeEntryRepository.find(timeEntryId);
+        if(null!= result) {
+            return new ResponseEntity<TimeEntry>(result, HttpStatus.MULTI_STATUS.OK);
+        }else{
+            return new ResponseEntity<TimeEntry>( HttpStatus.MULTI_STATUS.NOT_FOUND);
+        }
+    }
+    @GetMapping("/time-entries")
+    public ResponseEntity<List<TimeEntry>> list() {
+        List<TimeEntry> result=timeEntryRepository.list();
+        return new ResponseEntity<List<TimeEntry>>(result, HttpStatus.MULTI_STATUS.OK);
+    }
+    @PutMapping("/time-entries/{id}")
+    public ResponseEntity<TimeEntry> update(@PathVariable long id, @RequestBody TimeEntry timeEntry) {
+
+        TimeEntry result =timeEntryRepository.update(id, timeEntry);
+        if(null!= result) {
+            return new ResponseEntity<TimeEntry>(result, HttpStatus.OK);
+        }else{
+            return new ResponseEntity<TimeEntry>( HttpStatus.NOT_FOUND);
+        }
+
+    }
+    @DeleteMapping("/time-entries/{timeEntryId}")
+    public ResponseEntity delete(@PathVariable long timeEntryId) {
+        timeEntryRepository.delete(timeEntryId);
+        return new ResponseEntity<>( HttpStatus.NO_CONTENT);
+    }
 }
